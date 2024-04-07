7770155a4143a9d73bde060847a19d408054e1e6
JieningYu <jiening.yu@outlook.com>
2024-04-07 23:23:58 +0800

    idomatic code

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,59 @@
-use std::{fmt::Display, num::ParseIntError};
+//! Crate for finding bad Mac users.
+
+#![warn(missing_docs)]
+#![cfg(target_os = "macos")]
+
+use std::fmt::Display;
 
 use sysctl::{Ctl, Sysctl, SysctlError};
 
+/// Errors which will occur when checking Mac quality.
 #[derive(Debug)]
+#[non_exhaustive]
 pub enum Error {
-    NotPosixCompliant,
+    /// The macOS version is not compliant with POSIX.
+    NotPosix,
+    /// The Mac model is bad.
     BadMacModel,
-    SysctlError(SysctlError),
-    ParseOsVersionError,
+    /// Errors from [`sysctl`].
+    Sysctl(SysctlError),
+    /// Error when parsing macOS version.
+    ParseOsVersion,
+    /// Error variant that contains multiple errors.
+    Many(Vec<Self>),
 }
 
 impl Display for Error {
-    /// Human-readable error descriptions.
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::NotPosixCompliant=>write!(f, "Your macOS version is not compliant with POSIX, it is recommended to downgrade your macOS to a version prior to 14.4."),
-            Self::BadMacModel=>write!(f, "You have a bad taste, sell your Mac immediately and get a MacBook Pro (13-inch, M1, 2020)."),
-            Self::SysctlError(err)=>write!(f, "Sysctl error: {}.", err),
-            Self::ParseOsVersionError=>write!(f, "Your macOS version looks weird and can't be parsed."),
+            Error::NotPosix => write!(f, "your macOS version is not compliant with POSIX, it is recommended to downgrade your macOS to a version prior to 14.4"),
+            Error::BadMacModel => write!(f, "you have a bad taste, sell your Mac immediately and get a MacBook Pro (13-inch, M1, 2020)"),
+            Error::Sysctl(err) => write!(f, "sysctl error: {}", err),
+            Error::ParseOsVersion => write!(f, "your macOS version looks weird and can't be parsed"),
+            Error::Many(errs) => {
+                write!(f, "multiple errors: ")?;
+                for err in errs {
+                    write!(f, "{}, ", err)?;
+                }
+                Ok(())
+            },
         }
     }
 }
 
 impl From<SysctlError> for Error {
+    #[inline]
     fn from(value: SysctlError) -> Self {
-        Self::SysctlError(value)
+        Self::Sysctl(value)
     }
 }
 
-impl From<ParseIntError> for Error {
-    fn from(_value: ParseIntError) -> Self {
-        Self::ParseOsVersionError
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::Sysctl(err) => Some(err),
+            _ => None,
+        }
     }
 }
 
@@ -40,21 +63,26 @@ const KERN_OSPRODUCTVERSION: &str = "kern.osproductversion";
 /// Very bad machine.
 const PULP_MACHINE: &str = "MacBookPro16.1";
 
-#[cfg(target_os = "macos")]
-pub fn check() -> Result<(), Vec<Error>> {
-    let mut err: Vec<Error> = Vec::with_capacity(2);
-    let _ = check_posix().is_err_and(|e| {
-        err.push(e);
-        true
-    });
-    let _ = check_machine().is_err_and(|e| {
-        err.push(e);
-        true
-    });
-    if err.is_empty() {
+/// Checks whether this Mac is bad.
+///
+/// # Errors
+///
+/// - Errors if macOS version is equal to or newer than __`14.4`__, which is not POSIX-compliant.
+/// - Errors if the Mac model is `MacBookPro16.1`.
+pub fn check() -> Result<(), Error> {
+    let mut errs: Vec<Error> = Vec::with_capacity(2);
+    if let Err(err) = check_posix() {
+        errs.push(err);
+    }
+    if let Err(err) = check_machine() {
+        errs.push(err);
+    }
+    if errs.is_empty() {
         Ok(())
-    } else {
+    } else if let Some(err) = errs.pop().filter(|_| errs.is_empty()) {
         Err(err)
+    } else {
+        Err(Error::Many(errs))
     }
 }
 
@@ -66,21 +94,20 @@ fn check_posix() -> Result<(), Error> {
     let mut is_sonoma = false;
     for num in ver_split {
         if !is_sonoma {
-            match num.parse::<usize>()? {
+            match num.parse::<usize>().map_err(|_| Error::ParseOsVersion)? {
                 ..=13 => return Ok(()),
                 14 => is_sonoma = true,
-                _ => return Err(Error::NotPosixCompliant),
+                _ => return Err(Error::NotPosix),
             }
+        } else if let ..=3 = num.parse::<usize>().map_err(|_| Error::ParseOsVersion)? {
+            return Ok(());
         } else {
-            match num.parse::<usize>()? {
-                ..=3 => return Ok(()),
-                _ => return Err(Error::NotPosixCompliant),
-            }
+            return Err(Error::NotPosix);
         }
     }
 
     // Can't split version string by `.` because the loop doesn't run.
-    Err(Error::ParseOsVersionError)
+    Err(Error::ParseOsVersion)
 }
 
 fn check_machine() -> Result<(), Error> {
@@ -96,6 +123,6 @@ fn check_machine() -> Result<(), Error> {
 mod test {
     #[test]
     fn test_decency() {
-        crate::check().unwrap()
+        assert!(crate::check().is_ok())
     }
 }
